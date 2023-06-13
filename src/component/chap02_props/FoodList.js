9226d6a1c6fb2bea907d622821d3397efcbb94fc
jiwon <kjwn0241@naver.com>
2023-06-13 10:14:56 +0900

    230613 props의 활용

--- src/component/chap02_props/FoodList.js
+++ src/component/chap02_props/FoodList.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import FoodItem from './FoodItem';
+import SayHello from '../../SayHello';
+
 
 const FoodList = () => {
 
@@ -40,6 +42,15 @@ const FoodList = () => {
   return (
 
     <ul>
+
+        <SayHello>
+            <a href='https://www.naver.com'>네이버 링크</a>
+        </SayHello>
+        
+        <SayHello>
+            <a href='https://www.google.com'>구글 링크</a>
+        </SayHello>
+
        {
         food.foodList.map(f => <FoodItem foodName={f.Name} price={f.price} foodId={f.id}/>)
        }
